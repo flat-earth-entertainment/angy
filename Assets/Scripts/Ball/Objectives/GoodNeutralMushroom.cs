7d775961a86cf84696047e2645edebf8637a15be
TheSkyShaft <theskyshaft@hotmail.com>
2021-01-28 19:29:35 +0100

    Goal mushroom

--- Assets/Scripts/Ball/Objectives/GoodNeutralMushroom.cs
+++ Assets/Scripts/Ball/Objectives/GoodNeutralMushroom.cs
@@ -6,11 +6,20 @@ public class GoodNeutralMushroom : MonoBehaviour
 {
     public List<GameObject> fruit;
     private GameObject point;
+    public GameObject goal;
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Lemming"){
             if(point != null){
                 Destroy(point);
             }else{
+                int layermask = 1 << 15;
+                RaycastHit hit;
+                if(Physics.Raycast(transform.position + Vector3.up, Vector3.down, out hit, 10f, layermask)){
+                    print("HIT");
+                    GameObject hitObject = hit.collider.gameObject;
+                    Instantiate(goal, hitObject.transform.position, Quaternion.identity);
+                    Destroy(hitObject);
+                }
                 GetComponent<Renderer>().enabled = false;
             }
             point = Instantiate(fruit[other.transform.GetChild(0).GetComponent<Shooter>().playerId], transform.position + new Vector3(0,-1f,0), Quaternion.identity);
