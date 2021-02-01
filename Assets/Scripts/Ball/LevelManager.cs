8428f9f38173b7c184142d99a1287381ed4e0b57
unknown <glebzex@mail.ua>
2021-02-01 13:56:11 +0100

    Add basic mesh combiner

--- Assets/Scripts/Ball/LevelManager.cs
+++ Assets/Scripts/Ball/LevelManager.cs
@@ -1,10 +1,15 @@
-﻿public class LevelManager : Singleton<LevelManager>{
+﻿using Cysharp.Threading.Tasks;
+using MeshPhysics;
+
+public class LevelManager : Singleton<LevelManager>{
     
     private bool lateStart;
-    void Update() {
+    async void Update() {
         if(!lateStart){ // Activates only once, frame after start
             lateStart = true;
             // This one needs to be done after start since the thing its getting is created in start but can't be moved to awake
+            await UniTask.WaitUntil(() => FindObjectOfType<MeshCombiner>().Baked);
+            PredictionManager.instance.obstacles = FindObjectOfType<MeshCombiner>().transform.parent.gameObject;
             PredictionManager.instance.copyAllObstacles(); 
         }
     }
---
+++ Assets/Scripts/MeshPhysics/MeshCombiner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeshPhysics
+{
+    public class MeshCombiner : MonoBehaviour
+    {
+        public bool Baked { get; private set; }
+        
+        [SerializeField]
+        private GameObject obstacleParent;
+
+        private void Awake()
+        {
+            var combineInstance = new List<CombineInstance>();
+
+            foreach (Transform child in obstacleParent.transform)
+            {
+                if (child.TryGetComponent(out MeshFilter meshFilter))
+                {
+                    var instance = new CombineInstance
+                        {mesh = meshFilter.mesh, transform = meshFilter.transform.localToWorldMatrix};
+
+                    combineInstance.Add(instance);
+
+                    child.GetComponent<Collider>().enabled = false;
+                }
+            }
+
+            var combinedMesh = transform.GetComponent<MeshFilter>().mesh = new Mesh();
+            transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combineInstance.ToArray());
+
+            transform.GetComponent<MeshCollider>().sharedMesh = combinedMesh;
+
+            Baked = true;
+        }
+    }
+}
