068c3d51714585e8d1e26fa1cc294bed6362905c
TheSkyShaft <Theskyshaft@hotmail.com>
2021-02-01 16:52:37 +0100

    Fixed aiming snapping to other side

--- Assets/Scripts/Ball/Shooter.cs
+++ Assets/Scripts/Ball/Shooter.cs
@@ -32,7 +32,7 @@ public class Shooter : MonoBehaviour{
     private GameObject ballStorage;
     
     public bool activateShootingRetinae = true, active = true;
-    private int vertSnap = 36, horSnap = 35;
+    private int vertSnap = 180, horSnap = 270;
     // how many degrees the shooting retinae should snap. MUST add up to 360
     public int vertSnapAngle = 5, horSnapAngle = 5, greatSnapAngle = 30;
     private float snapCooldownTimer, vertSnapCooldownTimer, horSnapCooldownTimer;
