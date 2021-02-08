39921aac0ad1da9ce44fc5e97c4e639608a6257f
unknown <glebzex@mail.ua>
2021-02-08 16:29:09 +0100

    Add debug overlay

---
+++ Assets/Scripts/Debugs/DebugOverlay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Config;
+using Logic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Debugs
+{
+    public class DebugOverlay : MonoBehaviour
+    {
+        [SerializeField]
+        private Button expandAbility;
+
+
+        [SerializeField]
+        private Button noGravityAbility;
+
+        private void Awake()
+        {
+            expandAbility.onClick.AddListener(delegate
+            {
+                GameManager.CurrentTurnPlayer.Ability = GameConfig.Instance.AbilityValues.ExpandAbility;
+            });
+            noGravityAbility.onClick.AddListener(delegate
+            {
+                GameManager.CurrentTurnPlayer.Ability = GameConfig.Instance.AbilityValues.NoGravityAbility;
+            });
+        }
+    }
+}
