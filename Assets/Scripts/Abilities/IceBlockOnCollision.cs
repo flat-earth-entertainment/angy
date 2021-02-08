134d9db1a1fd44fffb68fa1d342cab175412b8f6
Gabe <glebzex@mail.ua>
2021-02-08 22:50:41 +0100

    Fix ice block ability

--- Assets/Scripts/Abilities/IceBlockOnCollision.cs
+++ Assets/Scripts/Abilities/IceBlockOnCollision.cs
@@ -14,7 +14,7 @@ namespace Abilities
 
         private void OnCollisionEnter(Collision other)
         {
-            if (other.collider.CompareTag("Lemming"))
+            if (other.collider.CompareTag("Lemming") && !other.transform.GetComponent<IceBlockOnCollision>())
             {
                 _initialDrag = other.rigidbody.drag;
                 other.rigidbody.drag = GameConfig.Instance.AbilityValues.IceBlockAbilityConfig.Drag;
@@ -23,10 +23,11 @@ namespace Abilities
                 _otherPlayerView.BecameStill += OnBecameStill;
 
                 _otherIceBlockOnCollision = other.gameObject.AddComponent<IceBlockOnCollision>();
+                Debug.Log($"Added ice from {transform.parent.name} to {other.transform.parent.name}");
 
                 _materials = _otherPlayerView.Materials;
                 _originalBodyMaterial = _materials[0];
-                Debug.Log(_originalBodyMaterial.name);
+                Debug.Log(_originalBodyMaterial.name, gameObject);
                 _materials[0] = GameConfig.Instance.AbilityValues.IceBlockAbilityConfig.IceMaterial;
                 _otherPlayerView.Materials = _materials;
             }
