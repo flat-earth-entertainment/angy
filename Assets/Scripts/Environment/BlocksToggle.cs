0986bb13bf319bd00f16ab31b841a158356b1604
unknown <glebzex@mail.ua>
2021-02-15 19:04:06 +0100

    Add toggle blocks

---
+++ Assets/Scripts/Environment/BlocksToggle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public class BlocksToggle : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] blocksToToggle;
+
+        [SerializeField]
+        private bool fireOnlyOnce;
+
+        private bool _beenToggled;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (fireOnlyOnce && _beenToggled)
+                return;
+
+            if (other.CompareTag("Lemming"))
+            {
+                foreach (var block in blocksToToggle)
+                {
+                    block.SetActive(block.activeSelf);
+                    _beenToggled = true;
+                }
+            }
+        }
+    }
+}
