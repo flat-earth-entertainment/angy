1cb016efe61a88b096285ff72f0ce9f1e9cb4e6c
Gabe <glebzex@mail.ua>
2021-03-23 18:45:45 +0200

    Fix respawn after acid pool

--- Assets/Scripts/Player/LastStandablePositionSetter.cs
+++ Assets/Scripts/Player/LastStandablePositionSetter.cs
@@ -4,17 +4,33 @@ namespace Player
 {
     public class LastStandablePositionSetter : MonoBehaviour
     {
+        private static int _notStandableLayerMask;
+
         [SerializeField]
         private LayerMask ground;
 
         [SerializeField]
         private PlayerView player;
 
+        private void Awake()
+        {
+            _notStandableLayerMask = LayerMask.GetMask("NotRespawnable");
+        }
+
         private void FixedUpdate()
         {
+            if (Physics.Raycast(transform.position + Vector3.up * 50, Vector3.down, 200, _notStandableLayerMask))
+            {
+                return;
+            }
+
             if (Physics.Raycast(transform.position + Vector3.up * 50, Vector3.down, out var hit, 200, ground))
             {
-                player.LastStandablePosition = hit.point;
+                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("IgnoredMap")
+                    || hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                {
+                    player.LastStandablePosition = hit.point;
+                }
             }
         }
     }
