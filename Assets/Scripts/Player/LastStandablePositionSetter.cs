1bfbf56e8b11ad2763a704bfb93df191f3353723
Gabe <glebzex@mail.ua>
2021-03-23 18:13:55 +0200

    Raised standable position

--- Assets/Scripts/Player/LastStandablePositionSetter.cs
+++ Assets/Scripts/Player/LastStandablePositionSetter.cs
@@ -12,7 +12,7 @@ namespace Player
 
         private void FixedUpdate()
         {
-            if (Physics.Raycast(transform.position, Vector3.down, out var hit, 50f, ground))
+            if (Physics.Raycast(transform.position + Vector3.up * 50, Vector3.down, out var hit, 200, ground))
             {
                 player.LastStandablePosition = hit.point;
             }
