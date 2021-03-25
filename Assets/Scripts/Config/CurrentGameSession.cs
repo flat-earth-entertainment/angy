3458d558b35391480b64641ce24bff2cabfb3f9a
Gabe <glebzex@mail.ua>
2021-03-25 22:05:02 +0200

    Change leaderboard not to show non-played maps

--- Assets/Scripts/Config/CurrentGameSession.cs
+++ Assets/Scripts/Config/CurrentGameSession.cs
@@ -30,8 +30,8 @@ namespace Config
 
     public readonly struct MapScore
     {
-        public readonly int Player1Score;
-        public readonly int Player2Score;
+        public readonly int? Player1Score;
+        public readonly int? Player2Score;
 
         public MapScore(int player1Score, int player2Score)
         {
