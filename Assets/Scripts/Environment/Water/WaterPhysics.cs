f98713b8112cbcf69a792a63298887988c4d287a
TheSkyShaft <Theskyshaft@hotmail.com>
2021-04-01 11:01:52 +0200

    Lemming Jesus

--- Assets/Scripts/Environment/Water/WaterPhysics.cs
+++ Assets/Scripts/Environment/Water/WaterPhysics.cs
@@ -4,25 +4,58 @@ using UnityEngine;
 
 public class WaterPhysics : MonoBehaviour
 {
-    public float baseDrag = 0.25f, waterDrag = 2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float baseDrag = 0.25f, waterDrag = 2f, bounceThreshold = 5;
+    [Tooltip("Should be above 3 or else bounce isn't strong enough")]
+    public float waterVerticalBounce = 5;
+    public bool iceAbility, bounce;
+    public GameObject ice;
+    private Abilities.IceBlockOnCollision player;
+    private bool delay;
     void OnTriggerEnter(Collider other){    // Must be rewritten if we end up wanting to kill players underwater
         if(other.tag == "Lemming"){
-            other.attachedRigidbody.drag = waterDrag;
+            WaterEvents(other);
         }
     }
     void OnTriggerExit(Collider other){
-        if(other.tag == "Lemming")
+        if(other.tag == "Lemming"){
             other.attachedRigidbody.drag = baseDrag;
+        }
+            
+    }
+    void WaterEvents(Collider other){
+        // Find out what to do
+            // is player ice?
+            if(other.gameObject.TryGetComponent(out Abilities.IceBlockOnCollision iceAbil)){
+                player = iceAbil;
+                iceAbility = true;
+            }
+
+            // is the player fast enough for a bounce?
+            if(other.attachedRigidbody.velocity.magnitude > bounceThreshold){
+                bounce = true;
+            }
+
+        // Do stuff
+        if(iceAbility){
+            ice.SetActive(true);
+            StartCoroutine("DisableIce");
+        }else if(bounce){
+            Vector3 vel = other.attachedRigidbody.velocity;
+            other.attachedRigidbody.velocity = new Vector3(vel.x, waterVerticalBounce, vel.z);
+        }else{  // Normal water logic
+            other.attachedRigidbody.drag = waterDrag;
+        }
+        iceAbility = bounce = false;
+    }
+    IEnumerator DisableIce(){
+        Player.PlayerView plView = player.transform.parent.GetComponent<Player.PlayerView>();
+        while (true)
+        {
+            if(plView.Ability.Finished){
+                break;
+            }
+            yield return null;
+        }
+        ice.SetActive(false);
     }
 }
