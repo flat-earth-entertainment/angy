d215258a2460f2f72a7347e3851bdb0907294228
Gabe <glebzex@mail.ua>
2021-06-09 22:26:44 +0300

    Add level editor

--- Assets/Scripts/Editor/Tools.cs
+++ Assets/Scripts/Editor/Tools.cs
@@ -1,9 +1,12 @@
+using System;
 using Ball.Objectives;
+using Config;
 using TMPro;
 using UnityEditor;
 using UnityEditor.Experimental.SceneManagement;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Editor
 {
@@ -21,7 +24,7 @@ namespace Editor
 
         private void FontReplace()
         {
-            _tmpFontAsset = (TMP_FontAsset) EditorGUILayout.ObjectField(_tmpFontAsset, typeof(TMP_FontAsset), true);
+            _tmpFontAsset = (TMP_FontAsset)EditorGUILayout.ObjectField(_tmpFontAsset, typeof(TMP_FontAsset), true);
 
             if (_tmpFontAsset != null && GUILayout.Button("Replace font"))
             {
@@ -50,11 +53,108 @@ namespace Editor
             }
         }
 
+        private GameObject _prefab;
+        private bool _shouldPaint;
+        private bool _randomizeY;
+        private bool _align;
+
+        private void OnEnable()
+        {
+            SceneView.duringSceneGui += OnSceneGui;
+        }
+
+        private void OnDisable()
+        {
+            SceneView.duringSceneGui -= OnSceneGui;
+        }
+
+        private Transform _drawnObjectsTransform;
+
+        private void OnSceneGui(SceneView sceneView)
+        {
+            if (!_shouldPaint || _prefab == null)
+                return;
+
+
+            if (_shouldPaint && Event.current.type == EventType.MouseUp && Event.current.button == 0)
+            {
+                if (Physics.Raycast(HandleUtility.GUIPointToWorldRay(Event.current.mousePosition), out var hit,
+                    Mathf.Infinity, LayerMask.GetMask("Ground")))
+                {
+                    var newObject = (GameObject)PrefabUtility.InstantiatePrefab(_prefab);
+                    Selection.activeObject = newObject;
+                    newObject.transform.position = hit.point;
+
+                    if (_drawnObjectsTransform == null)
+                    {
+                        var foundObject = GameObject.Find("Drawn Objects");
+                        if (foundObject != null)
+                        {
+                            _drawnObjectsTransform = foundObject.transform;
+                        }
+                        else
+                        {
+                            var newParent = new GameObject("Drawn Objects");
+                            _drawnObjectsTransform = newParent.transform;
+                        }
+                    }
+
+                    newObject.transform.parent = _drawnObjectsTransform;
+
+                    if (_align)
+                    {
+                        switch (_normalOrient)
+                        {
+                            case NormalOrient.X:
+                                newObject.transform.right = hit.point;
+                                break;
+                            case NormalOrient.Y:
+                                newObject.transform.up = hit.point;
+                                break;
+                            case NormalOrient.Z:
+                                newObject.transform.forward = hit.point;
+                                break;
+                        }
+                    }
+
+                    if (_randomizeY)
+                    {
+                        var rotation = newObject.transform.rotation;
+                        rotation = Quaternion.Euler(rotation.eulerAngles.x,
+                            Random.Range(-180, 181),
+                            rotation.eulerAngles.z);
+                        newObject.transform.rotation = rotation;
+                    }
+
+                    Undo.RegisterCreatedObjectUndo(newObject, "Spawned prefab");
+                    EditorUtility.SetDirty(newObject);
+                }
+            }
+
+            if (Event.current.type == EventType.Layout)
+                HandleUtility.AddDefaultControl(GUIUtility.GetControlID(GetHashCode(), FocusType.Passive));
+        }
+
+        public enum NormalOrient
+        {
+            X,
+            Y,
+            Z
+        }
+
+        private NormalOrient _normalOrient;
+
         private void OnGUI()
         {
-            // FontReplace();
+            _prefab = (GameObject)EditorGUILayout.ObjectField("Prefab:", _prefab, typeof(GameObject), false);
 
-            FindMushroomsWithAbilities();
+            _shouldPaint = GUILayout.Toggle(_shouldPaint, "Draw?");
+            _randomizeY = GUILayout.Toggle(_randomizeY, "Randomize Y rotation?");
+            _align = GUILayout.Toggle(_align, "Align with ground normal?");
+            if (_align)
+            {
+                _normalOrient = (NormalOrient)EditorGUILayout.EnumPopup("Orient axis", _normalOrient);
+            }
         }
 
         private void FindMushroomsWithAbilities()
